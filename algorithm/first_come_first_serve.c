ecc746df9305e71224a7f1c43fd9b61f1f6c1e08
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-11-25 08:38:14 +0530

    the folder for algorithm is made

---
+++ algorithm/first_come_first_serve.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+//======================================== Space for macro or global variables =================================//
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//======================================= Sapce for user-defined function ============================//
+
+
+
+//======================================== Space for main function ====================================//
+
+int main() {
+
+    exit(EXT_SUCCESS);
+}
