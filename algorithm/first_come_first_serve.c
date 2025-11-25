0568246e01ccc16063e7d2088253fffffab79eba
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-11-25 08:52:55 +0530

    The process data type is made

--- algorithm/first_come_first_serve.c
+++ algorithm/first_come_first_serve.c
@@ -1,7 +1,13 @@
+/*
+ * FCFS -> this is the algorithm that focuses over the arrival time of the processes and allocates accordingly 
+ * */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+
 //======================================== Space for macro or global variables =================================//
 
 #define EXT_SUCCESS 0
@@ -9,6 +15,14 @@
 
 //======================================= Sapce for user-defined function ============================//
 
+struct Process {
+    long long pid;
+    float arrival_time;
+    float brust_time;
+    float waiting_time;
+    float turnaround_time;
+    float response_time;
+};
 
 
 //======================================== Space for main function ====================================//
