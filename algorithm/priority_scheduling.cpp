a9687cb63f4e57eafbbec1b638c1d93f9ee872bc
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-11-29 13:23:29 +0530

    Added the file for priority scheduling with basic structure for code is done

---
+++ algorithm/priority_scheduling.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+#include <cstdlib>
+#include <unistd.h>
+#include <cstdbool>
+
+//============================= Space for macro or global variables =====================//
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+
+//================================ Space for user defined functions ========================//
+
+
+
+//============================ Space for main function only ===============================//
+
+int main() {
+
+    exit(EXT_SUCCESS);
+}
