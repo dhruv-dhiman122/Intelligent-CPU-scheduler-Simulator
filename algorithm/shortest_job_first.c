11380e8bd47eed19d7279b8b0c16eac03cb8a7de
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-12-09 18:25:14 +0530

    Added the file for shortest job first and wrote the standard code file format

---
+++ algorithm/shortest_job_first.c
@@ -0,0 +1,25 @@
+/*
+ * Aim of the file -> The files major aim is write code for shortest job first
+ * */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+//===================================================== Space for Macro or golbal variable ========================================//
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//==================================================== Space for user defined function ============================================//
+
+
+
+//================================================== Space for main function ========================================================//
+int main() {
+
+
+    exit(EXT_SUCCESS);
+}
