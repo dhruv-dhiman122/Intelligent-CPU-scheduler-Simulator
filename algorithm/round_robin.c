25bdcad9cd11219d69b58712a066038fad10d7cb
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-12-16 22:17:01 +0530

    completed the algorithm

--- algorithm/round_robin.c
+++ algorithm/round_robin.c
@@ -14,22 +14,76 @@ struct Process {
     float burst_time;
     float wait_time;
     float completion_time;
+    float remaining;
     float turnAround_time;
     int Time; // for time quantum
 };
 
 //======================================= Space for user defined function ============================================//
 
+void roundRobin(struct Process proc[], int n, int quantum) {
+    int time = 0, completed = 0;
 
+    for(int i = 0; i < n; i++) {
+        proc[i].remaining = proc[i].burst_time;
+        proc[i].wait_time = 0;
+    }
+
+    while(completed < n) {
+        int all_idle = 1;
+
+        for(int i = 0; i < n; i++) {
+            if(proc[i].remaining > 0) {
+                all_idle = 0;
+
+                int exec_time = (proc[i].remaining < quantum) ? proc[i].remaining : quantum;
+
+                printf("|P %d | %d",proc[i].pid, time+ exec_time);
+
+                for(int j = 0; j < n; j++) {
+                    if(j != i && proc[j].remaining > 0) {
+                        proc[j].wait_time += exec_time;
+                    }
+                }
+
+                proc[i].remaining -= exec_time;
+                time += exec_time;
+
+                if(proc[i].remaining == 0) {
+                    completed++;
+                    proc[i].turnAround_time = time;
+                }
+            }
+        }
+        if(all_idle) {
+            break;
+        }
+    }
+    printf("\n\nProcess\t Brust\tWait time\t Turn around time\n");
+    float total_wait = 0, total_turnaround = 0;
+
+    for(int i = 0; i < n; i++) {
+        printf("P%d\t%f\t%f\t%f\n",proc[i].pid, proc[i].burst_time, proc[i].wait_time, proc[i].turnAround_time);
+
+        total_wait += proc[i].wait_time;
+        total_turnaround += proc[i].turnAround_time;
+    }
+    printf("\nAvg waiting time: %.2f\n", total_wait / n);
+    printf("Avg turn around time is : %.2f\n", total_turnaround / n);
+}
 
 //======================================== Space for main function =================================================//
 int main() {
     
     //asking the user for number of process
     int numberProcess;
+    int time_quantum;
     
-    printf("Enter the number of process\n");
+    printf("enter the number of process\n");
     scanf("%d",&numberProcess);
+
+    printf("Enter the time for time quantum\n");
+    scanf("%d",&time_quantum);
 
     if(numberProcess <= 0) {
         fprintf(stderr, "PLEASE ENTER A VALID NUMBER!!\n");
@@ -57,7 +111,8 @@ int main() {
         process[i].turnAround_time = 0;
     }
     printf("Done!!\n");
-
+    
+    roundRobin(process, numberProcess, time_quantum);
 
     //freeing the allocated memory
     free(process);
