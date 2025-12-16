00272a5ed5d5718ad44ab74bc2956c11dbbebae3
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-12-16 21:08:26 +0530

    the code for asking the user input for arrival time and burst time

--- algorithm/round_robin.c
+++ algorithm/round_robin.c
@@ -39,6 +39,24 @@ int main() {
     //allocating memory to the process
     struct Process* process = (struct Process*)malloc(numberProcess * sizeof(struct Process));
 
+    for(int i = 0; i < numberProcess; i++) {
+        process[i].pid = i+1;
+        printf("\nEnter the arrival time for the process %d\t",i+1);
+        scanf("%f",&process[i].arrival_time);
+        
+        if(process[i].arrival_time <= 0.0) { // setting the arrival time for respective process as zero
+            printf("There was no arrival time set for %d. Thus, making it default value of zero\n",i+1);
+            process[i].arrival_time = 0;
+        }
+
+        printf("\nEnter the brust time for the process %d\t",i+1);
+        scanf("%f",&process[i].burst_time);
+        //making them zero
+        process[i].wait_time  = 0;
+        process[i].completion_time = 0;
+        process[i].turnAround_time = 0;
+    }
+    printf("Done!!\n");
 
 
     //freeing the allocated memory
